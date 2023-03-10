421d08f3dea6be7c4fec93126f93ac5ad26f507d
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2023-03-10 15:41:03 -0500

    protects cull index error

--- tmaven/controllers/cull.py
+++ tmaven/controllers/cull.py
@@ -12,7 +12,7 @@ class controller_cull(object):
 	def __init__(self,maven):
 		self.maven = maven
 
-	def cull_short(self,min_length):
+	def cull_short(self,min_length,current_index=None):
 		'''
 		Remove trajectories with number of kept-frames < threshold
 
@@ -28,14 +28,14 @@ class controller_cull(object):
 
 		keep = self.maven.data.post_list-self.maven.data.pre_list > min_length
 		msg = "Cull short traces < %d: kept %d out of %d = %f"%(min_length,keep.sum(),keep.size,keep.sum()/float(keep.size))
-		return self.cull_remove_traces(keep,msg)
+		return self.cull_remove_traces(keep,msg,current_index)
 
-	def cull_class(self,c):
+	def cull_class(self,c,current_index=None):
 		keep = self.maven.data.classes != c
 		msg = "Cull traces: kept %d out of %d = %f %%, class %d"%(keep.sum(),keep.size,keep.sum()/float(keep.size),c)
-		return self.cull_remove_traces(keep,msg)
+		return self.cull_remove_traces(keep,msg,current_index)
 
-	def cull_min(self,threshold):
+	def cull_min(self,threshold,current_index=None):
 		'''
 		Remove trajectories with one or more datapoints of value < min_threshold
 
@@ -50,10 +50,10 @@ class controller_cull(object):
 		'''
 		keep = np.min(self.maven.data.corrected,axis=(1,2)) > threshold
 		msg = "Cull traces: kept %d out of %d = %f %%, with a value less than %f"%(keep.sum(),keep.size,keep.sum()/float(keep.size),threshold)
-		return self.cull_remove_traces(keep,msg)
+		return self.cull_remove_traces(keep,msg,current_index)
 
 
-	def cull_max(self,threshold):
+	def cull_max(self,threshold,current_index=None):
 		'''
 		Remove trajectories with one or more datapoints of value > max_threshold
 
@@ -68,9 +68,9 @@ class controller_cull(object):
 		'''
 		keep = np.max(self.maven.data.corrected,axis=(1,2)) < threshold
 		msg = "Cull traces: kept %d out of %d = %f %%, with a value greater than %f"%(keep.sum(),keep.size,keep.sum()/float(keep.size),threshold)
-		return self.cull_remove_traces(keep,msg)
+		return self.cull_remove_traces(keep,msg,current_index)
 
-	def cull_remove_traces(self,mask,msg=''):
+	def cull_remove_traces(self,mask,msg='',current_index=None):
 		''' Keeps only those traces in `container_data` specified by mask
 
 		Parameters
@@ -90,9 +90,25 @@ class controller_cull(object):
 		'''
 		if mask.sum() == 0:
 			logger.error("cannot remove all traces")
-			return False
+			if current_index is None:
+				return False
+			return False, current_index
 		else:
+			## check current_index changes
+			if not current_index is None:
+				if mask.dtype == 'bool':
+					if mask[current_index] == True:
+						current_index = mask[:current_index].sum()
+					else:
+						current_index = 0
+				else:
+					if current_index in mask:
+						current_index = np.argmax(mask==current_index)
+					else:
+						current_index = 0
 			self.maven.data.order(mask)
 			self.maven.emit_data_update()
 			logger.info(msg)
-			return True
+			if current_index is None:
+				return True
+			return True,current_index
