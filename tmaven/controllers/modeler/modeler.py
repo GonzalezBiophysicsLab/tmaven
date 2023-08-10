fixes threshold normalization

tmaven/controllers/modeler/modeler.py
@@ -426,7 +426,7 @@ class controller_modeler(object):
 		mean = np.array([y[yclass==j].mean() for j in [0,1]]).astype('double')
 		var = np.array([y[yclass==j].var() for j in [0,1]]).astype('double')
 		frac = np.array([(yclass==j).sum() for j in [0,1]]).astype('double')
-		frac /= frac
+		frac /= frac.sum()
 
 		result = model_container(type='threshold',
 								 nstates=2,mean=mean,var=var,frac=frac,
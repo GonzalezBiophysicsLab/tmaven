dc38ec310bc1d8a7ecd554fd28d4bee3983fd6f0
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2023-08-09 22:15:04 -0400

    fixes threshold normalization

--- tmaven/controllers/modeler/modeler.py
+++ tmaven/controllers/modeler/modeler.py
@@ -426,7 +426,7 @@ class controller_modeler(object):
 		mean = np.array([y[yclass==j].mean() for j in [0,1]]).astype('double')
 		var = np.array([y[yclass==j].var() for j in [0,1]]).astype('double')
 		frac = np.array([(yclass==j).sum() for j in [0,1]]).astype('double')
-		frac /= frac
+		frac /= frac.sum()
 
 		result = model_container(type='threshold',
 								 nstates=2,mean=mean,var=var,frac=frac,
