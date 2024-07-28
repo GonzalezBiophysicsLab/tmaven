183fb068160010776c556bff76527c49572d9123
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2024-07-28 18:11:20 -0400

    adds config files and script toolbar

--- tmaven/interface/main_window.py
+++ tmaven/interface/main_window.py
@@ -11,6 +11,9 @@ matplotlib.use('Qt5Agg') ## forces Qt5 early on...
 
 import numpy as np
 from .resources import load_pixmap, load_icon
+import appdirs
+import json
+import os
 
 class_keys = {Qt.Key_1:1,Qt.Key_2:2,Qt.Key_3:3,Qt.Key_4:4,Qt.Key_5:5,Qt.Key_6:6,Qt.Key_7:7,Qt.Key_8:8,Qt.Key_9:9,Qt.Key_0:0}
 
@@ -27,6 +30,7 @@ class main_window(QMainWindow):
 		self.timer = None
 		self.lightdark_mode = 'light'
 		self.initialize_widgets()
+		self.lwd_load()
 		self.show()
 
 	def initialize_widgets(self):
@@ -82,7 +86,11 @@ class main_window(QMainWindow):
 		from .viewer_prefs import preferences_viewer
 		self.preferences_viewer = preferences_viewer(self)
 		
-
+		from .ui_scripts import scripts_toolbar
+		self.scripts_toolbar = scripts_toolbar(self)
+		self.addToolBar(Qt.TopToolBarArea, self.scripts_toolbar)
+		self.scripts_toolbar.hide()
+		
 		# #### smd info
 		# from .viewer_smd_info import smd_info_viewer
 		# self.smd_info_viewer = smd_info_viewer(self)
@@ -127,6 +135,45 @@ class main_window(QMainWindow):
 		self.data_update.connect(self.proc_data_update)
 		self.maven.emit_data_update()
 
+	def _config_file_getname(self,fname):
+		config_dir = appdirs.user_config_dir(appname="tmaven", appauthor="python")
+		if not os.path.exists(config_dir):
+			os.makedirs(config_dir)
+		config_file = os.path.join(config_dir, fname)
+		return config_file
+	
+	def config_file_read(self,fname):
+		config_file = self._config_file_getname(fname)
+		if not os.path.isfile(config_file):
+			return {}
+		try:
+			with open(config_file,'r') as f:
+				info = json.load(f)
+			return info ## dictionary
+		except:
+			return {}
+	
+	def config_file_update(self,fname,key,value):
+		info = self.config_file_read(fname)
+		info[key] = value
+		self.config_file_overwrite(fname,info)
+		
+	def config_file_overwrite(self,fname,dictionary):
+		config_file = self._config_file_getname(fname)
+		with open(config_file,'w') as f:
+			json.dump(dictionary, f)
+		
+	def lwd_load(self):
+		info = self.config_file_read('recent_files.json')
+		if 'lwd' in info:
+			self.lwd = info['lwd']
+		else:
+			self.lwd = os.getcwd()
+	
+	def lwd_update(self,lwd):
+		if os.path.exists(lwd):
+			self.lwd = lwd
+			self.config_file_update('recent_files.json','lwd',self.lwd)
 
 	def toggle_lock(self):
 		self.flag_locked = not self.flag_locked
@@ -170,7 +217,7 @@ class main_window(QMainWindow):
 		# self.menu_prefs.addAction('Save Preferences',self.preferences_viewer.save)
 		self.menu_file.addAction('Exit',self.quit,'Ctrl+Q')
 
-		self.menu_view.addAction('Reset GUI',self.default_session)
+		self.menu_view.addAction('Reset GUI',self.session_default)
 
 		self.menu_traj = self.menu_view.addMenu('Plot Type')
 		self.menu_traj.addAction('ND',lambda : self.plot_container.change_mode('ND'))
@@ -183,9 +230,9 @@ class main_window(QMainWindow):
 		self.menu_view.addAction(self.hdf5_viewer.action)
 		self.menu_view.addAction('Molecule Table',self.molecules_viewer.toggle,'Ctrl+T')
 		self.menu_view.addAction('Preferences',self.preferences_viewer.toggle,'Ctrl+P')
+		self.menu_view.addAction('Scripts Toolbar',self.scripts_toolbar.toggle,'Ctrl+R')
 
-
-		self.menu_scripts.addAction('Run Script',lambda:ui_scripts.run(self),'Ctrl+R')
+		self.menu_scripts.addAction('Run Script',lambda:ui_scripts.run(self))
 		self.menu_scripts.addAction('Run Input',lambda:ui_scripts.input_run(self))
 		# self.menu_experimental.addAction('Dwell Inversion',lambda:ui_experimental.dwell_inversion(self))
 
@@ -228,69 +275,66 @@ class main_window(QMainWindow):
 			self.proc_data_update()
 
 	def keyPressEvent(self,event):
-		kk = event.key()
-		from PyQt5.QtCore import Qt
-
-		
-		if kk == Qt.Key_G:
-			try:
-				self.plot_container.plot.ax[0,0].grid()
-				self.plot_container.plot.ax[1,0].grid()
+		try: ## don't want the user to blow it up
+			kk = event.key()
+			if kk == Qt.Key_G:
+					self.plot_container.plot.ax[0,0].grid()
+					self.plot_container.plot.ax[1,0].grid()
+					self.plot_container.plot.softredraw()
+					self.plot_container.plot.update_blits()
+			elif kk in [Qt.Key_Right, Qt.Key_Down]:
+				new_index = self.desired_index + 1
+				if new_index >= self.maven.data.nmol:
+					new_index = self.maven.data.nmol-1
+				self.change_index(new_index)
+			elif kk in [Qt.Key_Left, Qt.Key_Up]:
+				new_index = self.desired_index - 1
+				if new_index < 0:
+					new_index = 0
+				self.change_index(new_index)
+			elif kk in [Qt.Key_Minus,Qt.Key_Equal] and not self.flag_locked:
+				if kk == Qt.Key_Minus:
+					if self.maven.data.pre_list[self.index] > 0:
+						self.maven.data.pre_list[self.index] -= 1
+				else:
+					if self.maven.data.pre_list[self.index] < self.maven.data.ntime:
+						self.maven.data.pre_list[self.index] += 1
 				self.plot_container.plot.softredraw()
-				self.plot_container.plot.update_blits()
-			except:
-				pass
-		elif kk in [Qt.Key_Right, Qt.Key_Down]:
-			new_index = self.desired_index + 1
-			if new_index >= self.maven.data.nmol:
-				new_index = self.maven.data.nmol-1
-			self.change_index(new_index)
-		elif kk in [Qt.Key_Left, Qt.Key_Up]:
-			new_index = self.desired_index - 1
-			if new_index < 0:
-				new_index = 0
-			self.change_index(new_index)
-		elif kk in [Qt.Key_Minus,Qt.Key_Equal] and not self.flag_locked:
-			if kk == Qt.Key_Minus:
-				if self.maven.data.pre_list[self.index] > 0:
-					self.maven.data.pre_list[self.index] -= 1
-			else:
-				if self.maven.data.pre_list[self.index] < self.maven.data.ntime:
-					self.maven.data.pre_list[self.index] += 1
-			self.plot_container.plot.softredraw()
-		elif kk in [Qt.Key_BracketLeft,Qt.Key_BracketRight] and not self.flag_locked:
-			if kk == Qt.Key_BracketLeft:
-				if self.maven.data.post_list[self.index] > 0:
-					self.maven.data.post_list[self.index] -= 1
-			else:
-				if self.maven.data.post_list[self.index] < self.maven.data.ntime:
-					self.maven.data.post_list[self.index] += 1
-			self.plot_container.plot.softredraw()
-		# elif kk == Qt.Key_Space:
-		# 	self.maven.data.flag_ons[self.index] = not self.maven.data.flag_ons[self.index]
-		elif kk == Qt.Key_R and not self.flag_locked:
-			self.maven.data.pre_list[self.index] = 0
-			self.maven.data.post_list[self.index] = self.maven.data.ntime
-			self.plot_container.plot.softredraw()
-		elif kk == Qt.Key_C and not self.flag_locked:
-			self.maven.data.split_trace(self.index,self.maven.data.post_list[self.index])
-		elif kk == Qt.Key_V and not self.flag_locked:
-			self.change_index(self.maven.data.collect_trace(self.index))
-		elif kk == Qt.Key_P and not self.flag_locked:
-			try:
-				self.maven.photobleaching.calc_single_photobleach(self.index)
+			elif kk in [Qt.Key_BracketLeft,Qt.Key_BracketRight] and not self.flag_locked:
+				if kk == Qt.Key_BracketLeft:
+					if self.maven.data.post_list[self.index] > 0:
+						self.maven.data.post_list[self.index] -= 1
+				else:
+					if self.maven.data.post_list[self.index] < self.maven.data.ntime:
+						self.maven.data.post_list[self.index] += 1
 				self.plot_container.plot.softredraw()
-			except:
-				pass
-		elif event.key() in class_keys and not self.flag_locked:
-			new_class_ind = class_keys[kk]
-			self.maven.data.classes[self.index] = new_class_ind
-			self.update_mol_label()
-		else:
-			super().keyPressEvent(event)
-			return
-		self.molecules_viewer.update()
-		self.plot_container.plot.setFocus()
+			# elif kk == Qt.Key_Space:
+			# 	self.maven.data.flag_ons[self.index] = not self.maven.data.flag_ons[self.index]
+			elif kk == Qt.Key_R and not self.flag_locked:
+				self.maven.data.pre_list[self.index] = 0
+				self.maven.data.post_list[self.index] = self.maven.data.ntime
+				self.plot_container.plot.softredraw()
+			elif kk == Qt.Key_C and not self.flag_locked:
+				self.maven.data.split_trace(self.index,self.maven.data.post_list[self.index])
+			elif kk == Qt.Key_V and not self.flag_locked:
+				self.change_index(self.maven.data.collect_trace(self.index))
+			elif kk == Qt.Key_P and not self.flag_locked:
+				try:
+					self.maven.photobleaching.calc_single_photobleach(self.index)
+					self.plot_container.plot.softredraw()
+				except:
+					pass
+			elif event.key() in class_keys and not self.flag_locked:
+				new_class_ind = class_keys[kk]
+				self.maven.data.classes[self.index] = new_class_ind
+				self.update_mol_label()
+			else:
+				super().keyPressEvent(event)
+				return
+			self.molecules_viewer.update()
+			self.plot_container.plot.setFocus()
+		except:
+			pass
 		return
 
 	def update_selection_from_slider(self):
@@ -350,21 +394,23 @@ class main_window(QMainWindow):
 			self.index = self.desired_index
 			self.proc_data_update()
 
-
 	def screen_size(self):
 		"""
 		Returns an (width, height) tuple with the screen geometry.
 		"""
-
 		screen = QDesktopWidget().screenGeometry()
 		return screen.width(), screen.height()
 
-	def default_session(self):
+	def session_default(self):
 		self.plot_container.change_mode('smFRET')
 		self.size_window()
+		self.scripts_toolbar.clear_actions(update=False)
 		self.show()
 		# self.plot_container.plot.redrawplot() ## it is necessary to draw/resize after showing the interface, otherwise the DPI is messed up
 		self.change_theme_light() ## redrawplot called in this fxn too
+		self.config_file_overwrite('tmaven_config.json',{})
+		self.config_file_overwrite('recent_files.json',{})
+		self.lwd = os.getcwd()
 
 	def dark_theme(self):
 		dark_palette = QPalette()
@@ -440,70 +486,57 @@ class main_window(QMainWindow):
 		logger.info('trying to resize x{} y{} w{} h{}'.format(x,y,w,h))
 		self.move(x,y)
 
-	def save_session(self,session):
-		import os
-		import json
-		import appdirs
-		config_dir = appdirs.user_config_dir(appname="tmaven", appauthor="python")
-		config_file = os.path.join(config_dir, "tmaven_config.json")
-		if not os.path.exists(config_dir):
-			os.makedirs(config_dir)
-
-		with open(config_file, 'w') as f:
-			json.dump(session, f)
-
+	def session_save(self):
+		session = {
+			"window": {
+				"x": self.x(),
+				"y": self.y(),
+				"w": self.width(),
+				"h": self.height(),
+			},
+			"plot_mode": self.plot_mode,
+			"lightdark_mode" : self.lightdark_mode,
+		}
+		self.config_file_overwrite('tmaven_config.json',session)
 		session_str =  ",".join(f"{key}:{value}" for key, value in session.items())
-		logger.info('Saved session {} to {}'.format(session_str,config_file))
+		logger.info('Saved session {}'.format(session_str))
 
-	def restore_session(self):
-		import os
-		import json
-		import appdirs
-		config_dir = appdirs.user_config_dir(appname="tmaven", appauthor="python")
-		config_file = os.path.join(config_dir, "tmaven_config.json")
-
-		if not os.path.isfile(config_file):
-			logger.info('Cannot find a previous session to load')
-			self.default_session()
+	def session_restore(self):
+		session = self.config_file_read('tmaven_config.json')
+		if len(session.keys()) == 0:
+			self.session_default()
 			return
+		
+		x = session['window']['x'] if 'x' in session['window'] else None
+		y = session['window']['y'] if 'y' in session['window'] else None
+		w = session['window']['w'] if 'w' in session['window'] else None
+		h = session['window']['h'] if 'h' in session['window'] else None
 
-		try:
-			with open(config_file,'r') as f:
-				session = json.load(f)
-			session_str =  ",".join(f"{key}:{value}" for key, value in session.items())
-			logger.info('Loaded session data from {}.\nSession data - {}'.format(config_file,session_str))
-			x = session['window']['x'] if 'x' in session['window'] else None
-			y = session['window']['y'] if 'y' in session['window'] else None
-			w = session['window']['w'] if 'w' in session['window'] else None
-			h = session['window']['h'] if 'h' in session['window'] else None
+		lightdark_mode = session['lightdark_mode'] if 'lightdark_mode' in session else 'light'
+		if lightdark_mode == 'light':
+			palette = self.light_theme()
+			self.maven.prefs['plot.bg_color'] = '#FFFFFF'
+			self.lightdark_mode = 'light'
+		elif lightdark_mode == 'dark':
+			palette = self.dark_theme()
+			self.maven.prefs['plot.bg_color'] = '#353535'
+			self.lightdark_mode = 'dark'
 
-			lightdark_mode = session['lightdark_mode'] if 'lightdark_mode' in session else 'light'
-			if lightdark_mode == 'light':
-				palette = self.light_theme()
-				self.maven.prefs['plot.bg_color'] = '#FFFFFF'
-				self.lightdark_mode = 'light'
-			elif lightdark_mode == 'dark':
-				palette = self.dark_theme()
-				self.maven.prefs['plot.bg_color'] = '#353535'
-				self.lightdark_mode = 'dark'
+		plot_mode = session['plot_mode'] if 'plot_mode' in session else 'smFRET'
+		self.plot_container.change_mode(plot_mode)
+		self.size_window(x,y,w,h)
+		app = QApplication.instance()
+		app.setPalette(palette)
+		# app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+		app.processEvents()
+		self.show()
+		self.plot_container.plot.redrawplot() ## it is necessary to draw/resize after showing the interface, otherwise the DPI is messed up
 
-			plot_mode = session['plot_mode'] if 'plot_mode' in session else 'smFRET'
-			self.plot_container.change_mode(plot_mode)
-			self.size_window(x,y,w,h)
-			app = QApplication.instance()
-			app.setPalette(palette)
-			# app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
-			app.processEvents()
-			self.show()
-			self.plot_container.plot.redrawplot() ## it is necessary to draw/resize after showing the interface, otherwise the DPI is messed up
-		except:
-			logger.error('Failed loading previous session.')
-			self.default_session()
 
 	def show_log(self,event=None):
 		w = QMainWindow(parent=self)
 		w.setWindowTitle('Log')
-		w.resize(1000,200)
+		w.resize(1000,400)
 		w.closeEvent = lambda e: w.deleteLater()
 
 		te = QPlainTextEdit(parent=w)
@@ -547,21 +580,7 @@ class main_window(QMainWindow):
 				event.ignore()
 			return
 
-		session = {
-			"window": {
-				"x": self.x(),
-				"y": self.y(),
-				"w": self.width(),
-				"h": self.height(),
-			},
-			"plot_mode": self.plot_mode,
-			"lightdark_mode" : self.lightdark_mode,
-		}
-		self.save_session(session)
+		self.session_save()
 
 		logger.info("Quitting...")
-		# del self
-		# import sys
-		# sys.exit(0)
-		# return QMainWindow.closeEvent(self,event)
 		return super(type(self),self).closeEvent(event)
--- tmaven/interface/ui_scripts.py
+++ tmaven/interface/ui_scripts.py
@@ -1,12 +1,18 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from PyQt5.QtWidgets import QToolBar, QAction, QFileDialog
+from PyQt5.QtCore import Qt
+import appdirs
+import json
+import os 
+
 def run(gui):
 	from PyQt5.QtWidgets import QFileDialog
 	fname = QFileDialog.getOpenFileName(gui,'Choose script (.py) to run ','./')[0]
 	if fname == "":
 		return
-	gui.maven.scripts.run(fname,gui=gui)
+	run_file(gui,fname)
 
 def input_run(gui):
 	from PyQt5.QtWidgets import QInputDialog
@@ -19,7 +25,101 @@ def input_run(gui):
 		code = compile(text,'<string>','exec')
 		context = {'maven':gui.maven,'gui':gui} ## this will hold all of the elements in the plugin file... eg functions, global variables.
 		exec(code,context)
+		gui.maven.emit_data_update()
 	except Exception as e:
 		logger.error(str(e))
 		return False
 	return True
+
+def run_file(gui,fname):
+	gui.maven.scripts.run(fname, gui=gui)
+	gui.maven.emit_data_update()
+
+class scripts_toolbar(QToolBar):
+	def __init__(self, gui,):
+		super().__init__("Scripts Toolbar", parent=gui)
+		self.gui = gui
+
+		self.toggle_toolbar_action = QAction("Hide", self)
+		self.toggle_toolbar_action.triggered.connect(self._toggle)
+		self.addAction(self.toggle_toolbar_action)
+
+		self.clear_action = QAction("Clear", self)
+		self.clear_action.triggered.connect(self.clear_actions)
+		self.addAction(self.clear_action)
+
+		self.add_action = QAction("+", self)
+		self.add_action.triggered.connect(self.add_file_action)
+		self.addAction(self.add_action)
+		
+		self.addSeparator()
+
+		self.action_group = []
+		self.load_last()
+
+	def toggle(self):
+		# self.toggle_toolbar_action.trigger()
+		if self.isHidden():
+			self.show()
+		else:
+			self.hide()
+		
+	def _toggle(self, checked):
+		if checked:
+			self.show()
+		else:
+			self.hide()
+
+	def clear_actions(self,event=None,update=True):
+		for action in self.action_group[::-1]:
+			self.remove_action(action)
+		if update:
+			self.update_script_list()
+
+	def add_file_action(self):
+		options = QFileDialog.Options()
+		filenames, _ = QFileDialog.getOpenFileNames(self, "Select Files", self.gui.lwd, "Python (*.py)", options=options)
+		if len(filenames) > 0:
+			for filename in filenames:
+				self._add_file_action(filename)
+			lwd = os.path.dirname(filenames[-1])
+			self.gui.lwd_update(lwd)
+
+	def _add_file_action(self,filename,update=True):
+		file_action = QAction(filename.split('/')[-1], self)
+		file_action.setToolTip(filename)
+		file_action.triggered.connect(lambda checked, f=filename: self.run_script(f))
+		file_action.script_location = filename
+		self.addAction(file_action)
+		self.action_group.append(file_action)
+		if update:
+			self.update_script_list()
+	
+	def update_script_list(self):
+		scripts = [saction.script_location for saction in self.action_group]
+		self.gui.config_file_update('recent_files.json','scripts',scripts)
+
+	def load_last(self):
+		files = self.gui.config_file_read('recent_files.json')
+		if 'scripts' in files:
+			self.clear_actions(update=False)
+			for script in files['scripts']:
+				self._add_file_action(script,update=False)
+
+	def run_script(self, fname):
+		run_file(self.gui,fname)
+
+	def contextMenuEvent(self, event):
+		pass  # Do nothing to disable the default context menu
+
+	def mousePressEvent(self, event):
+		if event.button() == Qt.RightButton:
+			action = self.actionAt(event.pos())
+			if action and action not in [self.toggle_toolbar_action, self.add_action]:
+				self.remove_action(action)
+				self.update_script_list()
+		super().mousePressEvent(event)
+
+	def remove_action(self, action):
+		self.removeAction(action)
+		self.action_group.remove(action)
