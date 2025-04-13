1bbad63c1043fbe9e530eb732cc7f71e34d0b76d
Korak Ray <korakray@gmail.com>
2025-04-13 19:52:41 +0100

    added global normalise

--- tmaven/interface/ui_normalize.py
+++ tmaven/interface/ui_normalize.py
@@ -9,13 +9,21 @@ def build_menu(gui):
 	## Qt
 	from PyQt5.QtWidgets import QMenu
 	menu = QMenu('Normalize',gui)
-	#menu_filter = QMenu('Signal Filters',gui)
+	menu_ind = QMenu('Individual',gui)
+	menu_glob = QMenu('Global',gui)
 
 	### Corrections
 	#menu.addMenu(menu_filter)
 	menu.addAction('Reset',lambda : reset(gui))
-	menu.addAction('Individual Min-Max', gui.maven.normalize.normalize_minmax_ind)
 
+	menu_ind.addAction('Min-Max', gui.maven.normalize.normalize_minmax_ind)
+	menu_ind.addAction('CK Filter Min-Max', gui.maven.normalize.normalize_minmax_ckfilt_ind)
+
+	menu.addMenu(menu_ind)
+
+	menu_glob.addAction('Min-Max', gui.maven.normalize.normalize_minmax_glob)
+
+	menu.addMenu(menu_glob)
 
 	# from .stylesheet import ss_qmenu
 	# [m.setStyleSheet(ss_qmenu) for m in [menu,menu_filter]]
