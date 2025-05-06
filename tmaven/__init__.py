71cfa1afcaaa1038a821bc198c30e67a321067a2
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2025-05-06 12:04:39 -0400

    updates version

--- tmaven/__init__.py
+++ tmaven/__init__.py
@@ -2,6 +2,6 @@
 tMAVEN - Modeling, Analysis and Visualization ENvironment for N-dimensional single-molecule time series.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 from .app import run_app,setup_maven
